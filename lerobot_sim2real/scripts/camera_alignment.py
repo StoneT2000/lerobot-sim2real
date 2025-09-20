ecd408848dd438e9304d2c3390ded0a02ea95aa3
Jack Vial <vialjack@gmail.com>
2025-09-19 20:44:30 -0400

    Update camera alignment and update config for easy hec

--- lerobot_sim2real/scripts/camera_alignment.py
+++ lerobot_sim2real/scripts/camera_alignment.py
@@ -16,14 +16,16 @@ from mani_skill.utils import sapien_utils
 from dataclasses import dataclass
 import matplotlib.pyplot as plt
 
+
 @dataclass
 class Args:
-    env_id: str = "SO100GraspCube-v1"
+    env_id: str = "SO101GraspCubeLeRobotSim2Real-v1"
     """The environment id to train on"""
     env_kwargs_json_path: Optional[str] = None
     """Path to a json file containing additional environment kwargs to use."""
-    robot_uid: str = "so100"
+    robot_uid: str = "so101"
     """The robot UID to use (so100 or so101)"""
+
 
 def overlay_envs(sim_env, real_env):
     """
@@ -33,9 +35,9 @@ def overlay_envs(sim_env, real_env):
     """
     real_obs = real_env.get_obs()["sensor_data"]
     sim_obs = sim_env.get_obs()["sensor_data"]
-    assert sorted(real_obs.keys()) == sorted(
-        sim_obs.keys()
-    ), f"real camera names {real_obs.keys()} and sim camera names {sim_obs.keys()} differ"
+    assert sorted(real_obs.keys()) == sorted(sim_obs.keys()), (
+        f"real camera names {real_obs.keys()} and sim camera names {sim_obs.keys()} differ"
+    )
 
     overlaid_dict = sim_env.get_obs()["sensor_data"]
     overlaid_imgs = []
@@ -104,6 +106,7 @@ def update_camera(sim_env):
         print()
         help_message_printed = True
 
+
 camera_offset = torch.zeros(3, dtype=torch.float32)
 fov_offset = 0.0
 active_keys = set()
@@ -122,21 +125,25 @@ def on_key_release(event):
     global active_keys
     active_keys.discard(event.key)
 
-def main(args: Args):
-    real_robot = create_real_robot(uid=args.robot_uid)
-    real_robot.connect()
-    real_agent = LeRobotRealAgent(real_robot)
 
+def main(args: Args):
     env_kwargs = dict(
         obs_mode="rgb+segmentation",
         render_mode="sensors",
         reward_mode="none",
         # use larger camera resolution to make it easier to align. In training we won't use this however
-        sensor_configs=dict(width=512, height=512)
+        sensor_configs=dict(width=512, height=512),
     )
     if args.env_kwargs_json_path is not None:
         with open(args.env_kwargs_json_path, "r") as f:
-            env_kwargs.update(json.load(f))
+            data = json.load(f)
+            calibration_offset = data.pop("calibration_offset")
+            env_kwargs.update(**data)
+
+    real_robot = create_real_robot(uid=args.robot_uid)
+    real_robot.connect()
+    real_agent = LeRobotRealAgent(real_robot, calibration_offset=calibration_offset)
+
     sim_env = gym.make(
         args.env_id,
         **env_kwargs,
@@ -162,7 +169,9 @@ def main(args: Args):
     fig.canvas.mpl_connect("key_press_event", on_key_press)
     fig.canvas.mpl_connect("key_release_event", on_key_release)
 
-    print("Camera alignment: Move real camera to align with the sim camera, close figure to exit")
+    print(
+        "Camera alignment: Move real camera to align with the sim camera, close figure to exit"
+    )
     while True:
         overlaid_imgs = overlay_envs(sim_env, real_env)
         im.set_data(overlaid_imgs)
@@ -175,6 +184,7 @@ def main(args: Args):
         if not plt.fignum_exists(fig.number):
             print("The figure has been closed.")
             break
+
 
 if __name__ == "__main__":
     args = tyro.cli(Args)
