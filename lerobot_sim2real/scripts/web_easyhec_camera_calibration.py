2dfafd6375b7f450264bb0ff5b7ca8cc7ef28304
Jack Vial <vialjack@gmail.com>
2025-09-27 20:08:42 -0400

    Adjust gripper

--- lerobot_sim2real/scripts/web_easyhec_camera_calibration.py
+++ lerobot_sim2real/scripts/web_easyhec_camera_calibration.py
@@ -754,9 +754,9 @@ def main(args: SO101WebArgs):
         ).reshape(-1)[0]
     else:
         qpos_samples = [
-            np.array([0, 0, 0, np.pi / 2, np.pi / 2, 0.2]),
-            np.array([np.pi / 3, -np.pi / 6, 0, np.pi / 2, np.pi / 2, 0]),
-            np.array([-(np.pi / 3), -np.pi / 6, 0, np.pi / 2, np.pi / 2, 0]),
+            np.array([0, 0, 0, np.pi / 2, np.pi / 2, -1.0]),
+            np.array([np.pi / 3, -np.pi / 6, 0, np.pi / 2, np.pi / 2, -1.0]),
+            np.array([-(np.pi / 3), -np.pi / 6, 0, np.pi / 2, np.pi / 2, -1.0]),
             # np.array([-np.pi / 4, -np.pi / 6, np.pi / 6, np.pi / 2, np.pi / 2, 0.1]),
             # np.array([0, 0, 0, 0, np.pi / 2, 0.2]),
             # np.array([0, np.pi / 6, 0, 0, np.pi / 2, 0.2]),
